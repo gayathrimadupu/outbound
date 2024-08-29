974696120de0b774ce29b0d3a4342fc5164f6a46
Gayathri Madupu <gayathri.madupu@artihcus.com>
2024-08-29 07:00:08 +0000

    first commit

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,43 +1,91 @@
+
+
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/Device",
+	"sap/ui/model/json/JSONModel",
+	"sap/m/Popover",
+	"sap/m/Button",
+	"sap/m/library"
 ],
-function (Controller) {
+function (Controller,Device,JSONModel,Popover,Button,library) {
     "use strict";
 
     return Controller.extend("com.app.outbound.controller.View1", {
         onInit: function () {
-
+            
+			var oModel = new JSONModel(sap.ui.require.toUrl("com/app/outbound/model/data.json"));
+            this.getView().setModel(oModel);
+            this._setToggleButtonTooltip(!Device.system.desktop);
         },
-    
+ 
         onItemSelect: function (oEvent) {
-            var itemKey = oEvent.getParameter("item").getKey();
-            var navContainer = this.getView().byId("idNavContainer");
-            // Navigate to the corresponding page based on the selected key
-            switch (itemKey) {
-                case "OutboundKey":   //key
-                    navContainer.to(this.getView().createId("idscroolcontatiner1"));
-                    break;
-                case "PickingKey":
-                     navContainer.to(this.getView().createId("idscroll2"));
-                   break;
-                 case "PackingKey":
-                     navContainer.to(this.getView().createId("idscroll3"));
-                     break;
-                 case "LoadingKey":
-                     navContainer.to(this.getView().createId("idscroll4"));
-                     break;
-                 case "PickPointKey":
-                     navContainer.to(this.getView().createId("idscroll5"));
-                    break;
-                case "ConsumptionKey":
-                    navContainer.to(this.getView().createId("idscroll6"));
-                    break;
-                case "DistributionEquipmentKey":
-                    navContainer.to(this.getView().createId("idscroll7"));
-                    break;
-                default:
-                    break;
+            var oItem = oEvent.getParameter("item");
+            this.byId("pageContainer").to(this.getView().createId(oItem.getKey()));
+        },
+ 
+        handleUserNamePress: function (event) {
+            var oPopover = new Popover({
+                showHeader: false,
+                placement: PlacementType.Bottom,
+                content: [
+                    new Button({
+                        text: 'Feedback',
+                        type: ButtonType.Transparent
+                    }),
+                    new Button({
+                        text: 'Help',
+                        type: ButtonType.Transparent
+                    }),
+                    new Button({
+                        text: 'Logout',
+                        type: ButtonType.Transparent
+                    })
+                ]
+            }).addStyleClass('sapMOTAPopover sapTntToolHeaderPopover');
+ 
+            oPopover.openBy(event.getSource());
+        },
+ 
+        onSideNavButtonPress: function () {
+            var oToolPage = this.byId("toolPage");
+            var bSideExpanded = oToolPage.getSideExpanded();
+ 
+            this._setToggleButtonTooltip(bSideExpanded);
+ 
+            oToolPage.setSideExpanded(!oToolPage.getSideExpanded());
+        },
+ 
+        _setToggleButtonTooltip: function (bLarge) {
+            var oToggleButton = this.byId('sideNavigationToggleButton');
+            if (bLarge) {
+                oToggleButton.setTooltip('Large Size Navigation');
+            } else {
+                oToggleButton.setTooltip('Small Size Navigation');
             }
         },
+        
+         /** Notification text */
+         onNotificationAdminPress: function (oEvent) {
+            var oButton = oEvent.getSource(),
+                oView = this.getView();
+                debugger
+
+            // create popover
+            if (!this._pPopover) {
+                this._pPopover = this.loadFragment({
+                    name: "com.app.outbound.fragments.notificationAdmin"
+                }).then(function (oPopover) {
+                    oView.addDependent(oPopover);
+                    oPopover.bindElement("");
+                    return oPopover;
+                });
+            }
+            this._pPopover.then(function (oPopover) {
+                oPopover.openBy(oButton);
+            });
+        },
+
+		
     });
 });
